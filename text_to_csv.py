3939d9d6147cc7bdaebe27a3d63c2f3221900c0b
gustavozeferino <gustavomzeferino@gmail.com>
2025-03-16 10:42:58 -0300

    Processamento dos dados de negociação

--- text_to_csv.py
+++ text_to_csv.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import tkinter as tk
 from tkinter import filedialog
 import csv
@@ -39,20 +40,15 @@ def convert_to_csv(input_file, output_file):
             writer = csv.writer(output_data)  # Criar escritor do CSV
 
             fisrt_line = next(reader, None)
-            if check_file_header(fisrt_line):
-                pass
-            else:
+            if not check_file_header(fisrt_line):
                 raise InvalidFormatException
 
             # Ler e processar cada linha
             for line in reader:
-                
-                print(f"Lendo linha: {line}")  # Exibir a linha (opcional)
-                
-                csv_line = processed_data(line)
-                # Escrever a linha imediatamente no arquivo de saída
-                writer.writerow(csv_line)
-                print(f"Linha salva: {csv_line}")
+                if line[0:2] == '00':
+                    csv_line = processed_data(line)
+                    # Escrever a linha imediatamente no arquivo de saída
+                    writer.writerow(csv_line)
 
     except InvalidFormatException as e:
         print(e)
@@ -76,9 +72,35 @@ def check_file_header(data):
         return False
 
 def processed_data(data):
-    """Process the input data and return the processed data."""
-    # Processar os dados conforme necessário
-    processed_data = data
+    """Processamento dos dados de negociação."""
+    TIPREG=data[0:2]                                         # Tipo de registro
+    DTPREG=datetime.strptime(data[2:10], "%Y%m%d").date()    # Data do pregão
+    CODBDI=data[10:12]                                       # Código BDI  
+    CODNEG=(data[12:24]).strip()                             # Código de negociação do papel
+    TPMERC=data[24:27]                                       # Tipo de mercado
+    NOMRES=(data[27:39]).strip()                             # Nome resumido da empresa emissora do papel
+    ESPECI=(data[39:49]).strip()                             # Especificação do papel
+    PRAZOT=(data[49:52]).strip()                             # Prazo em dias do mercado a termo
+    MODREF=(data[52:56]).strip()                             # Moeda de referência
+    PREABE=float(data[56:69])/100                            # Preço de abertura
+    PREMAX=float(data[69:82])/100                            # Preço máximo
+    PREMIN=float(data[82:95])/100                            # Preço mínimo
+    PREMED=float(data[95:108])/100                           # Preço médio
+    PREULT=float(data[108:121])/100                          # Preço do último negócio
+    PREOFC=float(data[121:134])/100                          # Preço da melhor oferta de compra
+    PREOFV=float(data[134:147])/100                          # Preço da melhor oferta de venda
+    TOTNEG=int(data[147:152])                                # Número de negócios efetuados
+    QUATOT=int(data[152:170])                                # Quantidade total de títulos negociados
+    VOLTOT=float(data[170:188])                              # Volume total de títulos negociados
+    PREEXE=float(data[188:201])/100                          # Preço de exercício para opções
+    INDOPC=data[201:202]                                     # Indicador de correção de preços
+    DATVEN=datetime.strptime(data[202:210], '%Y%m%d').date() # Data do vencimento para opções
+    FATCOT=data[210:217]                                     # Fator de cotação do papel
+    PTOEXE=data[217:230]                                     # Preço de exercício para opções refer
+    CODISI=data[230:242]                                     # Código do papel no sistema ISIN ou código interno do papel
+    DISMES=data[242:245]                                     # Número de distribuição do papel
+    
+    processed_data = [TIPREG, DTPREG, CODBDI, CODNEG, TPMERC, NOMRES, ESPECI, PRAZOT, MODREF, PREABE, PREMAX, PREMIN, PREMED, PREULT, PREOFC, PREOFV, TOTNEG, QUATOT, VOLTOT, PREEXE, INDOPC, DATVEN, FATCOT, PTOEXE, CODISI, DISMES]
     return processed_data
 
 
